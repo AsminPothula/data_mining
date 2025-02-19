93ee1fa0b31889cfe967ce813b2eeb9644f8c3c1
Asmin Pothula <axp4488@mavs.uta.edu>
2025-02-19 22:24:38 +0000

    df dictionary created and fucntion to calculate idf values

--- p1.py
+++ p1.py
@@ -1,4 +1,6 @@
 import os
+import math
+from collections import defaultdict
 import nltk
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
@@ -17,22 +19,28 @@ for filename in os.listdir(corpusroot):
             each_doc_tokens = tokenizer.tokenize(doc) #tokenising each doc 
             all_tokens.append(each_doc_tokens)
 
-# print total token count after tokenization
-total_tokens = sum(len(doc) for doc in all_tokens)
-print("Total tokens after tokenization:", total_tokens)
-
 # stopword removal
 stop_words = set(stopwords.words('english'))
 filtered_tokens = [[word for word in doc if word not in stop_words] for doc in all_tokens]
-
-# print total token count after stopword removal
-total_filtered_tokens = sum(len(doc) for doc in filtered_tokens)
-print("Total tokens after stopword removal:", total_filtered_tokens)
 
 # stemming
 stemmer = PorterStemmer()
 stemmed_tokens = [[stemmer.stem(word) for word in doc] for doc in filtered_tokens]
 
-# print total token count after stemming
-total_stemmed_tokens = sum(len(doc) for doc in stemmed_tokens)
-print("Total tokens after stemming:", total_stemmed_tokens)
+df = defaultdict(int)  # token: df value
+N = len(stemmed_tokens) # total number of docs in corpus
+
+#get the df value for each (unique) token in each document and make the df dictionary
+for doc in stemmed_tokens:
+    unique_tokens = set(doc)  # unique tokens per document = non repeated 
+    for token in unique_tokens:
+        df[token] += 1  # increase that token's df value by 1 cause it appeared in the current doc 
+
+# calculate the idf value for the given token
+def getidf(token):
+    token = stemmer.stem(token)  
+    if token not in df:
+        return -1
+    return math.log10(N / df[token])  
+
+
